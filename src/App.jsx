c749c30f8ea60449e8cb1352411aad5f9f154418
sahil-r-sahu <sahilsahu8123@gmail.com>
2025-12-18 00:33:18 +0530

    Initial set-up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-red-500 text-4xl font-bold">
+        Sahil Verz
+      </h1>
+    </div>
+  );
+}
+
+export default App;
